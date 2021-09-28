2894cc9964e8002b6c97102f81fbbdc802e9e03e
QuantOpsNB <48415417+QuantOps@users.noreply.github.com>
2021-09-28 15:01:13 -0400

    Implemented initial Flask statements.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/aws_flask_app', methods=['GET'])
 def aws_flask_app(name):
-    print(f'Welcome to, {name}')
+    return "AWS_FLASK_APP!"
 
 
 if __name__ == '__main__':
-    aws_flask_app('AWS_FLASK_APP!')
+    app.run(host='0.0.0.0', port=8080)
